6213409b870b07573ac38786a4540adf57330b8c
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-29 22:34:28 -0500

    wrote the html routes to serve the notes.html file and index.html file

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(express.static('public'));
 
 
 //HTML Routes
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join (__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // API Routes
 
