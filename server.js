76c489405ab834c7a04d07e1f0a8e87bba6a4016
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-30 18:17:27 -0500

    Added error logging to the code since still cannot get notes to get written onto the page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.json());
@@ -22,18 +21,50 @@ app.get('/', (req, res) =>
 );
 
 // API Routes
-app.get('/api/notes', (req, res) =>
-     fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8')
-);
-
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8')
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2))
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(notes, null, 2),
+      (err) => {
+        if (err) throw err;
+        res.json(newNote);
+      }
+    );
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== id);
+
+    fs.writeFile(
+      path.join(__dirname, "db/db.json"),
+      JSON.stringify(notes, null, 2),
+      (err) => {
+        if (err) throw err;
+        res.json({ ok: true });
+      }
+    );
+  });
 });
 
 app.listen(PORT, () =>
