f057df8c46635844b098f3dcae0a2748fddd9068
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-29 22:13:00 -0500

    Added the pseudocode and port 3001

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+// need to find npm package that will give each note a unique id when it's saved
+
+const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:  true}));
 app.use(express.static('public'));
+
+
+//HTML Routes
+
+// API Routes
+
+
+// how to add the DELETE route from the frontend to the app
