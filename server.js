83c94b4bb9848e6034e67198d02fa8b16f89b194
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-07-29 22:09:29 -0500

    added requirements and middleware for parsing the JSOn and urlencoded data

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:  true}));
+app.use(express.static('public'));
